dbe86286e67692c9a0a40680c337bf74a6e5a64d
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-06-12 00:41:16 +0300

    Update homework5_1.py

--- homework5_1.py
+++ homework5_1.py
@@ -1,4 +1,7 @@
 class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
     def go_to(self, new_floor):
         if 0 < new_floor < self.number_of_floors:
             for i in range(1, new_floor + 1):
@@ -8,5 +11,5 @@ class House:
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(4)
+h1.go_to(6)
 h2.go_to(10)
